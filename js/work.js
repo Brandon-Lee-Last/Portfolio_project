649f41d7e1e90981e837c08eaa143a53a44bbcec
Brandon <63890355+Brandon-Lee-Last@users.noreply.github.com>
2020-08-17 19:55:39 +0200

    Add files via upload

---
+++ js/work.js
@@ -0,0 +1,7 @@
+$("#project1Btn").click(function () {
+  window.location.href = "https://github.com/Brandon-Lee-Last/FoodBlog_project";
+});
+
+$("#demo1Btn").click(function () {
+  window.location.href = "https://brandon-lee-last.github.io/FoodBlog_project/";
+});
